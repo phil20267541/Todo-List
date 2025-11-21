81493f48fba84e8d7cbd489c4ad16e4cb94298fa
Phil <phillip20267541@gmail.com>
2025-11-21 15:30:26 +0000

    Updates

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const app = Vue.createApp({
   data() {
     return {
-      header_title: "Phil20267541's ToDo List",
-      login_button: "Login",
-      login_overlay: false
+      headerTitle: "Phil20267541's To Do List",
+      loginButton: "Login",
+      loginOverlay: false,
+      navbarTitle: "My Lists",
+      lists: [{title: "List"},{title: "List"},{title: "List"},{title: "List"},{title: "List"},],
+      newListLabel: "+ New List",
+      loginLabel: "Login",
+      usernameLabel: "Username",
+      username: "",
+      passwordLabel: "Password",
+      password: "",
     }
   },
   methods: {
-    LoginModal() {
-      this.login_overlay = !this.login_overlay;
+    loginModal() {
+      this.loginOverlay = !this.loginOverlay;
+    },
+    login() {
+      console.log("hi")
     }
   }
 })
